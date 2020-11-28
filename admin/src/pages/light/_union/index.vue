0fff8bd57599769dcd7240bb609ece8eda9eb9bf
GeorgeZou <zousongqi@foxmail.com>
2020-11-28 11:18:32 +0800

    feat(app): app home page

--- admin/src/pages/light/_union/index.vue
+++ admin/src/pages/light/_union/index.vue
@@ -1,20 +1,20 @@
 <template>
   <ArrPanel class="home-page" v-if="currentApp" :title="currentApp.title" border>
-    <p>首页</p>
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="all" name="fourth">全部</el-tab-pane>
-      <el-tab-pane label="pc" name="first">PC页</el-tab-pane>
-      <el-tab-pane label="h5" name="second">H5</el-tab-pane>
-      <el-tab-pane label="form" name="third">表单</el-tab-pane>
+      <el-tab-pane label="全部" name="all"></el-tab-pane>
+      <el-tab-pane label="PC页" name="pc"></el-tab-pane>
+      <el-tab-pane label="H5" name="h5"></el-tab-pane>
+      <el-tab-pane label="表单" name="form"></el-tab-pane>
     </el-tabs>
     <div class="home-page-content">
-      <el-card :body-style="{ padding: '0px' }">
-        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <el-card class="app-item-card" v-for="(item, key) in list" :key="key" :body-style="{ padding: '0px' }" @click.native="handleTo(item)">
+        <img v-if="!item.image" :src="item.image" class="image">
+        <i else class="tefact-icon tf-icon-pages"></i>
         <div style="padding: 14px;">
-          <span>好吃的汉堡</span>
+          <span>{{ item.name }}</span>
           <div class="bottom clearfix">
-            <el-button type="text" class="button">修改</el-button>
-            <el-button type="text" class="button">分享</el-button>
+            <el-button type="text" class="button" @click.stop="handleEdit(item)">编辑</el-button>
+            <el-button type="text" class="button" @click.stop="handleEdit(item)">分享</el-button>
             <el-button type="text" class="button">删除</el-button>
           </div>
         </div>
@@ -26,15 +26,20 @@
 
 </style>
 <script lang="ts">
-  import { Vue, Component, namespace } from 'nuxt-property-decorator'
+  import { Vue, Component, namespace, Watch } from 'nuxt-property-decorator'
+  import { Application } from "~/services/common/entities/app"
 
   const app = namespace('app')
 
-  type List = {
-    name: string,
-    image: string,
-    link: string,
-    icon: string
+  type ListItemType = 'page' | 'form'
+
+  type ListItem = {
+    id?: string,
+    name?: string,
+    image?: string | null,
+    link?: string,
+    icon?: string,
+    type?: ListItemType
   }
 
   @Component({
@@ -42,16 +47,59 @@
   })
   export default class HomePage extends Vue {
     @app.Getter currentApp
+    @app.Action getAppDetails
 
     activeName: string = 'all'
-    list!: List
+    list: ListItem[] = []
+    app!: Application
+
+    @Watch('activeName', { immediate: true })
+    handleSwitchActive() {
+      if (!this.app) return
+      const res = [] as ListItem[]
+      switch (this.activeName) {
+        case "all":
+          this.app.pages.forEach(page => {
+            res.push({
+              id: page.id,
+              name: page.title,
+              image: page.head_image,
+              link: "",
+              type: "page",
+              icon: ""
+            })
+          })
+      }
+
+      this.list = res;
+    }
+
+    handleTo(page) {
+      this.$router.push(`/app/light/page/${page.id}`)
+    }
+
+    handleEdit(page) {
+      this.$router.push(`/app/light/${this.currentApp.id}/${page.id}/editor`)
+    }
 
     handleClick(e) {
       this.activeName = e
     }
 
+    async refresh() {
+      if (!this.currentApp) return
+      const res = await this.getAppDetails(this.currentApp.id)
+      this.app = res.data.data
+      this.handleSwitchActive()
+    }
+
+    @Watch("currentApp")
+    handleCurrentAppChange() {
+      this.refresh()
+    }
+
     mounted() {
-      console.log("this.current:", this.currentApp)
+      this.refresh()
     }
   }
 </script>
@@ -64,6 +112,32 @@
   .home-page-content {
     flex: 1;
     min-height: 100%;
+    flex-direction: row;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .app-item-card {
+    width: 235px;
+    margin: 10px 1.5%;
+    height: 347px;
+
+    &:hover {
+      border: 1px solid $thirdColor;
+    }
+
+    image {
+      height: 235px;
+    }
+
+    .tf-icon-pages {
+      height: 235px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 200px;
+      color: $thirdColor;
+    }
   }
 }
 </style>
