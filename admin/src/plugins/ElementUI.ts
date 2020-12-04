60b54dd4633ccfdc479fff06753cd5b60676da0e
GeorgeZou <zousongqi@foxmail.com>
2020-12-04 23:31:26 +0800

    chore(deploy): optimize bundle size

--- admin/src/plugins/ElementUI.ts
+++ admin/src/plugins/ElementUI.ts
@@ -1,7 +1,52 @@
 import { Vue } from 'nuxt-property-decorator'
-import Element from 'element-ui'
+// import Element from 'element-ui'
 
-Vue.use(Element)
+// Vue.use(Element)
+
+export default () => {
+  Vue.component('ElButton', () => import('element-ui/lib/button'))
+  Vue.component('ElSlider', () => import('element-ui/lib/slider'))
+  Vue.component('ElPagination', () => import('element-ui/lib/pagination'))
+  Vue.component('ElDropdown', () => import('element-ui/lib/dropdown'))
+  Vue.component('ElDropdownItem', () => import('element-ui/lib/dropdown-item'))
+  Vue.component('ElDropdownMenu', () => import('element-ui/lib/dropdown-menu'))
+  Vue.component('ElMenu', () => import('element-ui/lib/menu'))
+  Vue.component('ElMenuItem', () => import('element-ui/lib/menu-item'))
+  Vue.component('ElMenuItemGroup', () => import('element-ui/lib/menu-item-group'))
+  Vue.component('ElSubmenu', () => import('element-ui/lib/submenu'))
+  Vue.component('ElInput', () => import('element-ui/lib/input'))
+  Vue.component('ElInputNumber', () => import('element-ui/lib/input-number'))
+  Vue.component('ElForm', () => import('element-ui/lib/form'))
+  Vue.component('ElFormItem', () => import('element-ui/lib/form-item'))
+  Vue.component('ElOption', () => import('element-ui/lib/option'))
+  Vue.component('ElSelect', () => import('element-ui/lib/select'))
+  Vue.component('ElTimeSelect', () => import('element-ui/lib/time-select'))
+  Vue.component('ElRadio', () => import('element-ui/lib/radio'))
+  Vue.component('ElContainer', () => import('element-ui/lib/container'))
+  Vue.component('ElRow', () => import('element-ui/lib/row'))
+  Vue.component('ElCol', () => import('element-ui/lib/col'))
+  Vue.component('ElUpload', () => import('element-ui/lib/upload'))
+  Vue.component('ElCheckbox', () => import('element-ui/lib/checkbox'))
+  Vue.component('ElCheckboxButton', () => import('element-ui/lib/checkbox-button'))
+  Vue.component('ElTabs', () => import('element-ui/lib/tabs'))
+  Vue.component('ElTabPane', () => import('element-ui/lib/tab-pane'))
+  Vue.component('ElCard', () => import('element-ui/lib/card'))
+  Vue.component('ElTag', () => import('element-ui/lib/tag'))
+  Vue.component('ElTable', () => import('element-ui/lib/table'))
+  Vue.component('ElTableColumn', () => import('element-ui/lib/table-column'))
+  Vue.component('ElButton', () => import('element-ui/lib/button'))
+  Vue.component('ElAvatar', () => import('element-ui/lib/avatar'))
+  Vue.component('ElDialog', () => import('element-ui/lib/dialog'))
+  Vue.component('ElPopover', () => import('element-ui/lib/popover'))
+  Vue.component('ElBadge', () => import('element-ui/lib/badge'))
+  Vue.component('ElTooltip', () => import('element-ui/lib/tooltip'))
+  Vue.component('ElButtonGroup', () => import('element-ui/lib/button-group'))
+  Vue.component('ElColorPicker', () => import('element-ui/lib/color-picker'))
+  Vue.component('ElCollapse', () => import('element-ui/lib/collapse'))
+  Vue.component('ElCollapseItem', () => import('element-ui/lib/collapse-item'))
+  Vue.component('ElPopconfirm', () => import('element-ui/lib/popconfirm'))
+}
+
 // import { Vue } from 'nuxt-property-decorator'
 // import {
 //   Pagination,
@@ -100,7 +145,12 @@ Vue.use(Element)
 //
 // Vue.use(Loading.directive);
 //
-// Vue.prototype.$loading = Loading.service;
-// Vue.prototype.$alert = MessageBox.alert;
-// Vue.prototype.$confirm = MessageBox.confirm;
-// Vue.prototype.$message = Message;
+
+import { Loading } from "element-ui";
+import { Message } from "element-ui";
+import { MessageBox } from "element-ui";
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$message = Message;
