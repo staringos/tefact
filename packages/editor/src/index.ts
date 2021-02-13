d3bb4c6f4697c93d6ce09f0dcc658b2e498dbe84
GeorgeZou <zousongqi@foxmail.com>
2021-02-13 17:44:35 +0800

    feat(all): config for publish

---
+++ packages/editor/src/index.ts
@@ -0,0 +1,3 @@
+import Editor from "./component/index.vue";
+
+export default Editor;
--- packages/editor/vue.config.js
+++ packages/editor/vue.config.js
@@ -2,22 +2,49 @@
 const path = require("path");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nodeExternals = require("webpack-node-externals");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require("webpack");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 
 const isLib = process.env.BD === "lib";
 const isProd = process.env.NODE_ENV === "production";
 
-// function addStyleResource(rule) {
-//   rule
-//     .use("style-resource")
-//     .loader("style-resources-loader")
-//     .options({
-//       patterns: [path.resolve(__dirname, "./src/assets/styles/index.scss")]
-//     });
-// }
+const plugins = [
+  new webpack.IgnorePlugin({
+    resourceRegExp: /^\.\/locale$/,
+    contextRegExp: /moment$/
+  })
+];
+
+if (isLib) {
+  plugins.push(
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  );
+}
 
 module.exports = {
-  configureWebpack(config) {
-    config.externals = isProd && isLib ? [nodeExternals()] : {};
+  publicPath: "/",
+  outputDir: isLib ? "lib" : "dist",
+  productionSourceMap: true,
+  filenameHashing: false,
+  lintOnSave: process.env.NODE_ENV !== "production",
+  chainWebpack: config => {
+    config.plugins.delete("prefetch");
+    if (isLib) {
+      config.entry("main").clear();
+      config.entry("main").add("./src/index.ts");
+    }
+
+    config.resolve.alias.set("@", resolve("src"));
+  },
+  configureWebpack: {
+    plugins,
+    externals: isProd && isLib ? [nodeExternals()] : {}
   },
   css: {
     loaderOptions: {
